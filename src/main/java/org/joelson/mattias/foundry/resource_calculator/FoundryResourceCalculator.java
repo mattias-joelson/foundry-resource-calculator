f134715887b40c01a59e26ca6bbde8f0a7b8c4ae
Mattias Joëlson <mattias@joelson.org>
2025-06-18 14:15:59 +0200

    Package input in a single file.

--- src/main/java/org/joelson/mattias/foundry/resource_calculator/FoundryResourceCalculator.java
+++ src/main/java/org/joelson/mattias/foundry/resource_calculator/FoundryResourceCalculator.java
@@ -1,15 +1,12 @@
 package org.joelson.mattias.foundry.resource_calculator;
 
-import org.joelson.mattias.foundry.resource_calculator.model.JsonItem;
-import org.joelson.mattias.foundry.resource_calculator.model.JsonMaker;
+import org.joelson.mattias.foundry.resource_calculator.model.JsonCalculatorConfig;
 import org.joelson.mattias.foundry.resource_calculator.model.JsonReader;
-import org.joelson.mattias.foundry.resource_calculator.model.JsonRecipe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Map;
 
 public class FoundryResourceCalculator {
 
@@ -18,25 +15,19 @@ public class FoundryResourceCalculator {
     private static final int ERROR_EXIT_STATUS = 1;
 
     public static void main(String[] args) throws IOException {
-        if (args.length != 6) {
+        if (args.length != 2) {
             exitWithError(String.format("""
                             Usage:
-                            %s recipes.json jsonItems.json jsonMakers.json recipes.json goal_makers.json goal_recipe.json goals.json
+                            %s calculator_config.json goals.json
                             
-                            jsonItems.json        File containing item types in Foundry game.
-                            jsonMakers.json       File containing jsonMakers for Foundry game jsonItems.
-                            recipes.json      File containing recipes for Foundry game jsonItems.
-                            goal_makers.json  File containing which kind of jsonMakers to use.
-                            goal_recipe.json  File containing which recipe to use for jsonItems having multiple recipes.
+                            calculator_config.json  File containing items, recipes and makers in the Foundry game
                             goals.json        File containing production goals.""",
                     FoundryResourceCalculator.class));
         }
 
         JsonReader reader = new JsonReader();
-
-        Map<String, JsonItem> jsonItems = reader.readJsonItems(Path.of(args[0]));
-        Map<String, JsonMaker> jsonMakers = reader.readJsonMakers(Path.of(args[1]));
-        Map<String, JsonRecipe> jsonRecipes = reader.readJsonRecipes(Path.of(args[2]));
+        JsonCalculatorConfig jsonCalculatorConfig = reader.readJsonCalculatorConfig(Path.of(args[0]));
+        logger.info(jsonCalculatorConfig.toString());
     }
 
     private static void exitWithError(String msg) {
---
+++ src/main/java/org/joelson/mattias/foundry/resource_calculator/model/JsonCalculatorConfig.java
@@ -0,0 +1,22 @@
+package org.joelson.mattias.foundry.resource_calculator.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record JsonCalculatorConfig(
+        List<JsonMaker> makers,
+        List<JsonItem> items,
+        List<JsonRecipe> recipes)
+{
+    @JsonCreator
+    public JsonCalculatorConfig(
+            @JsonProperty(value = "makers", required = true) List<JsonMaker> makers,
+            @JsonProperty(value = "items", required = true) List<JsonItem> items,
+            @JsonProperty(value = "recipes", required = true) List<JsonRecipe> recipes) {
+        this.makers = makers;
+        this.items = items;
+        this.recipes = recipes;
+    }
+}
