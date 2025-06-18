479a573e451574409a834b3d268d432709a2c03b
Mattias Joëlson <mattias@joelson.org>
2025-06-18 03:14:22 +0200

    Inital setup

---
+++ src/main/java/org/joelson/mattias/foundry/resource_calculator/FoundryResourceCalculator.java
@@ -0,0 +1,8 @@
+package org.joelson.mattias.foundry.resource_calculator;
+
+public class FoundryResourceCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
